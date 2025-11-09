1155c6de3d164f067878e4be985c14bcd97ac566
Alexey <simindey.alexey@gmail.com>
2025-11-09 10:37:23 +0600

    mock: created mock data to work

---
+++ src/mock/users.mock.ts
@@ -0,0 +1,6 @@
+export const users = [
+    {id: 1, name: "Иван", surname: "Иванов", patronymic: "Иванович", phone: "+78005553535", email: "ivanchik@email.com", work_expirience: 10, work_place: "4 корпус", isActive: true},
+    {id: 2, name: "Владислав", surname: "Фаткудинов", patronymic: "Константинович", phone: "+77777777777", email: "megavlad@email.com", work_expirience: 2, work_place: "Главный корпус", isActive: true},
+    {id: 3, name: "Никита", surname: "Калмыков", patronymic: "Дмитриевич", phone: "+74242424242", email: "nikita@email.com", work_expirience: 1, work_place: "Главный корпус", isActive: true},
+    {id: 4, name: "Арина", surname: "Ангальд", patronymic: "Юрьевна", phone: "+79999999999", email: "irina@email.com", work_expirience: 1, work_place: "Главный корпус", isActive: true},
+]
